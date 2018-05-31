3d7812817c2792b224dbcb449287fd3789c95707
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-31 23:41:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,12 +37,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == true
+    if Song.find_by_name(name) == true
        return self.find_by_name(name)
     else
       self.create_by_name(name)
     end
-  end
 
   end
 
