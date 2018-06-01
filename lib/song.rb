c82218726fae28a583b8d1e139ec5149183825b1
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-01 00:05:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,17 +17,17 @@ class Song
   end
 
   def self.new_by_name(name)
-    @song = self.new
-    @song.name = name
+    song = self.new
+    song.name = name
     #@@all << @song
-    return @song
+    return song
   end
 
   def self.create_by_name(name)
-    @song = self.new
-    @song.name = name
-    @@all << @song
-    return @song
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
   end
 
   def self.find_by_name(name)
